fbadcfd607125223d9013b35c9fe91c39d3162b3
Kurt Strecker <ksphotopro@gmail.com>
2020-06-23 00:02:45 -0400

    border hexes always show outter border walls

--- scripts/main.js
+++ scripts/main.js
@@ -17,18 +17,54 @@ for (let i = 0; i < 10; i++) {
       const threshold = 5;
       switch (k) {
         case 0:
-          baseRectangle.className = random > threshold ? 'rectangle' : 'no_border';
+          baseRectangle.className = random > threshold ? 'rectangle both_border' : 'rectangle';
           baseRectangle.dataset.rotation = 0;
+          if (j === 9) {
+            baseRectangle.className = 'rectangle right_border';
+          } else if (j === 0) {
+            baseRectangle.className = 'rectangle left_border';
+          }
           break;
         case 1:
-          baseRectangle.className = random > threshold ? 'rectangle rectangle_1' : 'no_border rectangle_1';
+          baseRectangle.className = random > threshold ? 'rectangle both_border rectangle_1' : 'rectangle no_border rectangle_1';
           baseRectangle.dataset.rotation = 60;
+          if (j === 9  && i % 2 !== 0) {
+            baseRectangle.className = 'rectangle right_border rectangle_1';
+          } else if (j === 0 && i % 2 === 0) {
+            baseRectangle.className = 'rectangle left_border rectangle_1';
+          }
+
+          if (i === 0) {
+            baseRectangle.className = 'rectangle left_border rectangle_1';
+          } else if (i === 9) {
+            baseRectangle.className = 'rectangle right_border rectangle_1';
+          }
+          
+          // if (j === 9 && i ===9) {
+          //   baseRectangle.className = 'rectangle both_border rectangle_1';
+          // }
           break;
         case 2:
-          baseRectangle.className = random > threshold ? 'rectangle rectangle_2' : 'no_border rectangle_1';
+          baseRectangle.className = random > threshold ? 'rectangle both_border rectangle_2' : 'rectangle no_border rectangle_2';
           baseRectangle.dataset.rotation = 120;
+          if (j === 9 && i % 2 !== 0) {
+            baseRectangle.className = 'rectangle right_border rectangle_2';
+          } else if (j === 0 && i % 2 === 0) {
+            baseRectangle.className = 'rectangle left_border rectangle_2';
+          }
+
+          if (i === 0 && i % 2 === 0) {
+            baseRectangle.className = 'rectangle right_border rectangle_2';
+          } else if (i === 9) {
+            baseRectangle.className = 'rectangle left_border rectangle_2';
+          }
+
+          if ((i === 0 && j === 0) || (i === 9 && j ===9)) {
+            baseRectangle.className = 'rectangle both_border rectangle_2';
+          }
           break;
       }
+      
       hex.appendChild(baseRectangle);
     }
     row.appendChild(hex);
